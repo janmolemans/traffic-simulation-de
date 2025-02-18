bd27be6a0710ed5dd1eb10c0c6b9b33f1b90aec5
Jan Molemans <jan.molemans@gmail.com>
2025-02-19 00:36:49 +0100

    moved clutter to clutter

--- js/paths.js
+++ js/paths.js
@@ -151,51 +151,6 @@ function trajFromSpec(u,trajP){
 
 
 
-// old format called, e.g.,
-// coords=[trajFromSpec_x(u,trajP),trajFromSpec_y(u,trajP)]
-// or xPhys=trajFromSpec_x(u,trajP)
-
-function trajFromSpec_x(u,trajP){
-  
-  var iSegm=0;
-  while((u>trajP.u[iSegm+1])&&(iSegm+1<trajP.xCenter.length)){iSegm++;} 
-
-  var curv=(trajP.phi[iSegm+1]-trajP.phi[iSegm])
-    /(trajP.u[iSegm+1]-trajP.u[iSegm]);
-  var straightSegm=(Math.abs(curv)<1e-6);
-  var r=(straightSegm) ? 1e6 : 1./curv; // with sign
-
-  var x=(straightSegm)
-      ? trajP.x[iSegm] + (u-trajP.u[iSegm])*Math.cos(trajP.phi[iSegm])
-      : trajP.xCenter[iSegm]
-      + r*Math.sin(trajP.phi[iSegm]+curv*(u-trajP.u[iSegm]));
-
-  return x;
-}
-
-function trajFromSpec_y(u,trajP){
-  
-  var iSegm=0;
-  while((u>trajP.u[iSegm+1])&&(iSegm+1<trajP.xCenter.length)){iSegm++;} 
-
-  var curv=(trajP.phi[iSegm+1]-trajP.phi[iSegm])
-    /(trajP.u[iSegm+1]-trajP.u[iSegm]);
-  var straightSegm=(Math.abs(curv)<1e-6);
-  var r=(straightSegm) ? 1e6 : 1./curv; // with sign
-
-  var y=(straightSegm)
-      ? trajP.y[iSegm] + (u-trajP.u[iSegm])*Math.sin(trajP.phi[iSegm])
-      : trajP.yCenter[iSegm]
-      - r*Math.cos(trajP.phi[iSegm]+curv*(u-trajP.u[iSegm]));
-
-  return y;
-}
-
-			       
-  
-
-
-
 
 
 
